b0eee8e05ef4e2635d60657d20c5eb87085e8981
sinan-lp <sinan.ahammed@logicplum.com>
2025-11-05 01:52:31 +0530

    sd

--- landing/urls.py
+++ landing/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('terms/', views.terms, name='terms'),
+    path('privacy/', views.privacy, name='privacy'),
 ]
